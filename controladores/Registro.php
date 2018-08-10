ee23c3e7bbd7594743e5371cce37f6c2945bb93b
ever07 <42252637+ever07@users.noreply.github.com>
2018-08-11 00:16:32 +0200

    formulario
    estilos

--- controladores/Registro.php
+++ controladores/Registro.php
@@ -1,19 +1,13 @@
 <?php
-//require '../modelos/conexion.php';
+require '../modelos/conexion.php';
 require'../modelos/Modelo_registro.php';
-require'../vistas/view_registro.html';
+require'../vistas/view_regalumno.html';
 
 class registro{
 
 	 public function add_users(){
-             
-    $obj= new modelo_reg();
-    $usuario=$_GET['username'];
-    $password=$_GET['password'];
-    $nombre=$_GET['nombre'];
-    $apellidos=$_GET['apellidos'];
-    $obj->insert_registro($usuario, $password, $nombre, $apellidos);
-    
+             $ins= mysqli_query($link, $query)
+     
     }
 
 }
